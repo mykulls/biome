4dfd6438347086122a55f6c388b3e2172662517c
Michael <michshi8@gmail.com>
2021-05-06 18:33:37 -0700

    Initial Setup
    Creation of foundational project files, with client and server combined.

---
+++ server/models/listing.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create schema for listings
+const ListingSchema = new Schema({
+  name: {
+    type: String,
+    required: 'The name field is required',
+  },
+  address: {
+    type: String,
+    required: 'The address field is required',
+  },
+  city: {
+    type: String,
+    required: 'The city field is required',
+  },
+  zip: {
+    type: Number,
+    required: 'The zip code field is required',
+  },
+})
+
+//create model for todo
+const Listing = mongoose.model('listing', ListingSchema);
+
+module.exports = Listing;
---
+++ server/routes/api.js
@@ -0,0 +1,35 @@
+const express = require ('express');
+const router = express.Router();
+const Listing = require('../models/listing');
+
+router.get('/listings', (req, res, next) => {
+  Listing.find({})
+    .then(data => res.json(data))
+    .catch(next);
+});
+
+router.post('/listings', (req, res, next) => {
+  const body = req.body;
+  if (body.name && body.address && body.city && body.zip){
+    Listing.create(req.body)
+      .then(data => res.json(data))
+      .catch(next);
+  }
+  else {
+    Object.keys(Listing.schema.obj).forEach((key) => {
+      if (!(key in body)) {
+        res.json({
+          error: `The ${key} field is empty`,
+        });
+      }
+    });
+  }
+});
+
+router.delete('/listings/:id', (req, res, next) => {
+  Listing.findOneAndDelete({"_id": req.params.id})
+    .then(data => res.json(data))
+    .catch(next);
+});
+
+module.exports = router;
