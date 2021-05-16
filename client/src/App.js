0284a282688932c680bd8858fb658835f07989cd
danielsdai <69875004+danielsdai@users.noreply.github.com>
2021-05-15 18:42:47 -0700

    Basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,104 +1,12 @@
-import React from 'react';
-import Navbar from "./components/Navbar/Navbar";
-import Card from './components/Card'
 import './App.css';
-
+import Navbar from './navbar.js';
+import Posts from './posts.js';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
-      
-      <div className="container"> 
-        <div className="row"> 
-          <Card
-            title='10996 Roebling'
-            imageUrl='https://images.rentable.co/64123/40937454/large.jpg'
-            price='$1250'
-            body1='2 people'
-            body2='1 bedroom, 1 bathroom'
-            body3='kitchen, laundry, onsite parking'
-            body4='less than 1 mile from campus'
-          />
-
-          <Card
-            title='10996 Roebling'
-            imageUrl='https://images.rentable.co/64123/40937454/large.jpg'
-            price='$1250'
-            body1='2 people'
-            body2='1 bedroom, 1 bathroom'
-            body3='kitchen, laundry, onsite parking'
-            body4='less than 1 mile from campus'
-          />
-
-          <Card
-            title='10996 Roebling'
-            imageUrl='https://images.rentable.co/64123/40937454/large.jpg'
-            price='$1250'
-            body1='2 people'
-            body2='1 bedroom, 1 bathroom'
-            body3='kitchen, laundry, onsite parking'
-            body4='less than 1 mile from campus'
-          />
-
-          <Card
-            title='10996 Roebling'
-            imageUrl='https://images.rentable.co/64123/40937454/large.jpg'
-            price='$1250'
-            body1='2 people'
-            body2='1 bedroom, 1 bathroom'
-            body3='kitchen, laundry, onsite parking'
-            body4='less than 1 mile from campus'
-          />
-        </div>
-
-      </div>
-
-      <div className="container"> 
-        <div className="row"> 
-          <Card
-            title='10996 Roebling'
-            imageUrl='https://images.rentable.co/64123/40937454/large.jpg'
-            price='$1250'
-            body1='2 people'
-            body2='1 bedroom, 1 bathroom'
-            body3='kitchen, laundry, onsite parking'
-            body4='less than 1 mile from campus'
-          />
-
-          <Card
-            title='10996 Roebling'
-            imageUrl='https://images.rentable.co/64123/40937454/large.jpg'
-            price='$1250'
-            body1='2 people'
-            body2='1 bedroom, 1 bathroom'
-            body3='kitchen, laundry, onsite parking'
-            body4='less than 1 mile from campus'
-          />
-
-          <Card
-            title='10996 Roebling'
-            imageUrl='https://images.rentable.co/64123/40937454/large.jpg'
-            price='$1250'
-            body1='2 people'
-            body2='1 bedroom, 1 bathroom'
-            body3='kitchen, laundry, onsite parking'
-            body4='less than 1 mile from campus'
-          />
-
-          <Card
-            title='10996 Roebling'
-            imageUrl='https://images.rentable.co/64123/40937454/large.jpg'
-            price='$1250'
-            body1='2 people'
-            body2='1 bedroom, 1 bathroom'
-            body3='kitchen, laundry, onsite parking'
-            body4='less than 1 mile from campus'
-          />
-        </div>
-
-      </div>
-
+      <Posts />
     </div>
   );
 }
