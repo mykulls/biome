6c55c16531184420320a12e355da93f7e95959cd
Michael Shi <michshi8@gmail.com>
2021-05-30 18:34:05 -0700

    Remove console.log from exports

--- client/src/exports.js
+++ client/src/exports.js
@@ -3,5 +3,5 @@ import * as Realm from 'realm-web';
 const app = new Realm.App({ id: process.env.REACT_APP_REALM_APP_ID });
 const origin = process.env.NODE_ENV === 'production' ? '' : `http://localhost:${process.env.REACT_APP_PORT}`;
 // replace production with the deploy link later
-console.log(process.env.REACT_APP_PORT);
+
 export { app, Realm, origin };
