ddceff73e972225dda9c73dfcd81ff72ed68cebb
Michael Shi <michshi8@gmail.com>
2021-06-03 16:04:22 -0700

    Fix doubled post saving in user

--- client/src/NewPost/NewPost.js
+++ client/src/NewPost/NewPost.js
@@ -81,13 +81,6 @@ export default function NewPost() {
             .catch((e) => {
               console.log(e.message);
             });
-          const newPost = { $push: { posts: res.data._id } };
-          axios.patch(`${origin}/updateUser/${app.currentUser.id}`, newPost)
-            .then(() => {
-            })
-            .catch((e) => {
-              console.log(e.message);
-            });
         })
         .catch((e) => {
           setSubmitting(false);
