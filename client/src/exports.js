a1839d92261231f601ffcf38a980e2cc1dd02106
Michael Shi <michshi8@gmail.com>
2021-06-04 20:19:36 -0700

    Deployment server env variable added

--- client/src/exports.js
+++ client/src/exports.js
@@ -1,7 +1,6 @@
 import * as Realm from 'realm-web';
 
 const app = new Realm.App({ id: process.env.REACT_APP_REALM_APP_ID });
-const origin = process.env.NODE_ENV === 'production' ? '' : `http://localhost:${process.env.REACT_APP_PORT}`;
-// replace production with the deploy link later
+const origin = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_SERVER : `http://localhost:${process.env.REACT_APP_PORT}`;
 
 export { app, Realm, origin };
